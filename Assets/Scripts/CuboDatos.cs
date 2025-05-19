5e5329eecfc6228d9c3296475a1124014fb458fa
Juan Cruz Sabio <sabiojuancruz2@gmail.com>
2025-05-19 10:42:42 -0300

    tipito con mano barriles y cajas

---
+++ Assets/Scripts/CuboDatos.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboDatos : MonoBehaviour
+{
+    public GameObject cubo;
+    public Transform mano;
+
+    private bool activo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activo == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                transform.SetParent(mano);
+                cubo.transform.position = mano.position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "FirstPersonCharacter")
+        {
+            activo = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "FirstPersonCharacter")
+        {
+            activo = false;
+        }
+    }
+}
